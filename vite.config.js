10ea51f8d39743b44efbdf79527631c60552be67
Andrii Rykal <andreuka79@gmail.com>
2024-12-27 12:10:00 +0200

    base rename

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/huawei-test-task/',
+  base: '/Random-quote/',
   plugins: [
     vue(),
   ],
